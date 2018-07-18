b9f228ecabbc9116e67e3a4863c25fec8e0283c8
Cian <detec.azul@gmail.com>
2018-07-18 18:32:27 +0100

    Testing deploy with guest credentials

--- routers/index_router.js
+++ routers/index_router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const recipesRouter = require('./recipes_router.js');
 
-MongoClient.connect('', (err, client) => {
+MongoClient.connect('mongodb://guest:guestPassword1@ds243041.mlab.com:43041/heroku_zbrlbmhv', (err, client) => {
   const db = client.db('recetarium');
   const recipesCollection = db.collection('recipes');
   router.use('/api/recipes', recipesRouter(recipesCollection));
