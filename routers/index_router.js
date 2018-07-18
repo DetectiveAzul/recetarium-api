aab4b0fc5860be07b8aaa905a5a9c98211dbff4e
Cian <detec.azul@gmail.com>
2018-07-18 18:08:52 +0100

    Configuring remote mongodb

--- routers/index_router.js
+++ routers/index_router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const recipesRouter = require('./recipes_router.js');
 
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+MongoClient.connect('mongodb://heroku_zbrlbmhv:heroku_zbrlbmhv:43041/heroku_zbrlbmhv', (err, client) => {
   const db = client.db('recetarium');
   const recipesCollection = db.collection('recipes');
   router.use('/api/recipes', recipesRouter(recipesCollection));
