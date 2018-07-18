ef7a4ebece912a23de549b0ebf92a193d7f97938
Cian <detec.azul@gmail.com>
2018-07-18 18:35:58 +0100

    Added mLab db name

--- routers/index_router.js
+++ routers/index_router.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const recipesRouter = require('./recipes_router.js');
 
 MongoClient.connect('mongodb://guest:guestPassword1@ds243041.mlab.com:43041/heroku_zbrlbmhv', (err, client) => {
-  const db = client.db('recetarium');
+  const db = client.db('heroku_zbrlbmhv');
   const recipesCollection = db.collection('recipes');
   router.use('/api/recipes', recipesRouter(recipesCollection));
 });
