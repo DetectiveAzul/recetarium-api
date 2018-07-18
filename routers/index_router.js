ef849b1019727a4de7359a881a4e622afb6eade5
Cian <detec.azul@gmail.com>
2018-07-18 18:17:20 +0100

    Removed test credentials from index_router. Changing these and putting them on a gitignored file

--- routers/index_router.js
+++ routers/index_router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const recipesRouter = require('./recipes_router.js');
 
-MongoClient.connect('mongodb://heroku_zbrlbmhv:aassdd123@ds243041.mlab.com:43041/heroku_zbrlbmhv', (err, client) => {
+MongoClient.connect('', (err, client) => {
   const db = client.db('recetarium');
   const recipesCollection = db.collection('recipes');
   router.use('/api/recipes', recipesRouter(recipesCollection));
