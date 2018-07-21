962895623c48ac48120d279bc7cd563b69e213f1
DetectiveAzul <detec.azul@gmail.com>
2018-07-21 14:26:50 +0100

    Enabling CORS on Server side for API requests

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ let port = process.env.PORT || 8080;
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(parser.json());
 app.use(indexRouter);
 
